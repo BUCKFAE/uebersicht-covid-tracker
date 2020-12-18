f3b0d3d3025e3e86a57ed95f6924a6f4fc7cf707
Julian Schubert <schubertjulian1999@gmail.com>
2020-12-18 18:05:05 +0100

    Storing data for offline use

--- covid.py
+++ covid.py
@@ -1,6 +1,10 @@
 
 import requests
 import string
+import os
+from os import path
+from datetime import datetime
+import json
 
 URL = "https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_Landkreisdaten/FeatureServer/0/query?where=1%3D1&outFields=GEN,cases,deaths,cases7_per_100k,last_update,EWZ,cases_per_population,cases_per_100k,death_rate&returnGeometry=false&outSR=4326&f=json"
 
@@ -8,10 +12,25 @@ URL = "https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_La
 CITIES_TO_DISPLAY = ['Neumarkt', 'Würzburg', 'Haßberge']
 
 def get_data():
-    
-    # API request
-    response = requests.get(URL).json()
 
+    # Creating the data dir if if does not exist
+    if not path.exists("covid-tracker/data"):
+        os.mkdir("covid-tracker/data")
+
+    date = datetime.today().strftime('%Y-%m-%d')
+    print("Current Date: {}".format(date))
+
+    if not path.exists("covid-tracker/data/{}.json".format(date)):
+        with open("covid-tracker/data/{}.json".format(date), "w") as df:
+            print("yeet")
+            # API request
+            response = requests.get(URL).json()
+            json.dump(response, df)
+    else:
+        print("antiyeet")
+        with open("covid-tracker/data/{}.json".format(date)) as df:
+            response = json.load(df)
+    
     # Contains the data for all cities
     cities = response['features']
 
@@ -24,7 +43,7 @@ def get_data():
 
     # Iterating over all cities
     for current_city in cities:
-        
+
         curr = current_city['attributes']
 
         # City attributes
@@ -38,11 +57,11 @@ def get_data():
         total_cases += cases
         total_deaths += deaths
         total_incidence +=incidence
-        
+
         entry = {'cases': cases, 'incidence': incidence, 'deaths': deaths, 'population': population}
 
         # If two cities have the same name we pick the one with the higher population
-        if any(c for c in CITIES_TO_DISPLAY if c in curr['GEN']): 
+        if any(c for c in CITIES_TO_DISPLAY if c in curr['GEN']):
             if name in data:
                 if data[name]['population'] < population:
                     data[name] = entry
@@ -63,7 +82,7 @@ def create_table():
 
     # Table head
     table="<table><tr><th></th><th>Fälle (Gesamt)</th><th>Inzidenz</th><th>Tote</th></tr>"
-    
+
     # Adding data to the table
     for table_row in sorted(data.keys()):
         table += "<tr>"
@@ -72,7 +91,7 @@ def create_table():
         table += "<td>{}</td>".format(int(data[table_row]['incidence']))
         table += "<td>{}</td>".format(data[table_row]['deaths'])
         table += "</tr>"
-    
+
     # End of table
     table += "</table>"
 
@@ -80,7 +99,7 @@ def create_table():
 
 
 if __name__ == "__main__":
-    
+
     # Creating a table and printing it
     table = create_table()
     print(table)
